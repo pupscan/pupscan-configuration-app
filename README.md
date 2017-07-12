defb74fbb3dc6c293aec6b86eee776184c2c3de3
Thibaut Mottet <thibaut.mottet@7next.fr>
2017-07-12 16:24:45 +0200

    Init

---
+++ README.md
@@ -0,0 +1,15 @@
+# Pupscan configuration application
+
+## How to
+
+* `yarn install`
+* `yarn start`
+* Go to http://localhost:8080/
+
+For now only dropbox it's working.
+
+## Configure your IntelliJ
+
+* Set the JavaScript version: **Preference > Language & Framework > JavaScript**, select `ECMAScript 6`
+* Remove semicolon: **Preference > Editor > Code Style > JavaScript > Punctuation**, select `Don't use`
+* Install mustache plugins
---
+++ index.js
@@ -0,0 +1,25 @@
+const mainUrl = process.env.VIRTUAL_HOST ? process.env.VIRTUAL_HOST : "http://localhost:8080"
+
+const dropboxUrl = "https://www.dropbox.com/oauth2/authorize?response_type=code&redirect_uri=" + mainUrl + "/qr&client_id=ib7yg6jhh4xb7m3&state=dropbox"
+
+const express = require("express")
+const mustacheExpress = require('mustache-express')
+const qrCode = require("qrcode-npm")
+
+const app = express()
+app.engine('mustache', mustacheExpress())
+app.set('view engine', 'mustache')
+app.set('views', __dirname + '/views')
+
+app.get('/', (req, res) => res.render('index.mustache', {dropboxUrl: dropboxUrl}))
+
+app.get('/wifi', (req, res) => res.render('wifi.mustache'))
+
+app.get('/qr', function (req, res) {
+    let qr = qrCode.qrcode(4, 'L')
+    qr.addData(JSON.stringify(req.query))
+    qr.make()
+    res.render('qr.mustache', {qrcode: qr.createImgTag(4)})
+})
+
+app.listen(8080)
