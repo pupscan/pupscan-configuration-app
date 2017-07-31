273c16179f55943a8b39efce12b6d695904f4706
Thibaut Mottet <thibaut.mottet@7next.fr>
2017-07-31 15:57:32 +0200

    Keep secret

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const mainUrl = process.env.VIRTUAL_HOST ? 'https://' + process.env.VIRTUAL_HOST : 'http://localhost:8080'
+
+
 const dropbox = {
-    clientId: 'ib7yg6jhh4xb7m3',
-    url: 'https://www.dropbox.com/oauth2/authorize?response_type=code&redirect_uri=' + mainUrl + '/qr&client_id=ib7yg6jhh4xb7m3&state=dropbox'
+  clientId: process.env.DROPBOX_CLIENT_ID,
+  url: 'https://www.dropbox.com/oauth2/authorize?response_type=code&redirect_uri=' + mainUrl + '/qr&client_id=' + process.env.DROPBOX_CLIENT_ID + '&state=dropbox'
 }
 const google = {
-    clientId: '289383398542-oluetan6ds67bdtbgj5mb850jdjhmfbo.apps.googleusercontent.com',
-    clientSecret: '4D7nk4yui_P92_sxuGuHKnZI',
-    url: 'https://accounts.google.com/o/oauth2/auth?response_type=code&scope=profile%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcloudprint&approval_prompt=force&access_type=offline&client_id=289383398542-oluetan6ds67bdtbgj5mb850jdjhmfbo.apps.googleusercontent.com&redirect_uri=' + mainUrl + '/printer'
+  clientId: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  url: 'https://accounts.google.com/o/oauth2/auth?response_type=code&scope=profile%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcloudprint&approval_prompt=force&access_type=offline&client_id='+ process.env.GOOGLE_CLIENT_ID +'&redirect_uri=' + mainUrl + '/printer'
 }
 
 const express = require('express')
@@ -25,39 +27,39 @@ app.get('/', (req, res) => res.render('index.mustache', {dropboxUrl: dropbox.url
 app.get('/wifi', (req, res) => res.render('wifi.mustache'))
 
 app.get('/printer', function (req, res) {
-    promiseRequest({
-        method: 'POST',
-        url: 'https://www.googleapis.com/oauth2/v3/token',
-        form: {
-            code: req.query.code,
-            redirect_uri: mainUrl + '/printer',
-            client_id: google.clientId,
-            client_secret: google.clientSecret,
-            grant_type: 'authorization_code'
-        },
-        json: true
-    })
-        .then((response) => {
-            let printClient = new cloudPrint({
-                clientId: google.clientId,
-                clientSecret: google.clientSecret,
-                accessToken: response.access_token,
-                refreshToken: response.refresh_token
-            })
-            printClient.getPrinters()
-                .then((printers) => {
-                    let printerList = printers.map((printer) => {
-                        printer.isOnline = (printer.status === 'ONLINE')
-                        return printer
-                    }).filter((printer) => printer.id !== '__google__docs')
-                    res.render('printer.mustache', {
-                        printers: printerList,
-                        accessToken: response.access_token,
-                        refreshToken: response.refresh_token
-                    })
-                })
+  promiseRequest({
+    method: 'POST',
+    url: 'https://www.googleapis.com/oauth2/v3/token',
+    form: {
+      code: req.query.code,
+      redirect_uri: mainUrl + '/printer',
+      client_id: google.clientId,
+      client_secret: google.clientSecret,
+      grant_type: 'authorization_code'
+    },
+    json: true
+  })
+    .then((response) => {
+      let printClient = new cloudPrint({
+        clientId: google.clientId,
+        clientSecret: google.clientSecret,
+        accessToken: response.access_token,
+        refreshToken: response.refresh_token
+      })
+      printClient.getPrinters()
+        .then((printers) => {
+          let printerList = printers.map((printer) => {
+            printer.isOnline = (printer.status === 'ONLINE')
+            return printer
+          }).filter((printer) => printer.id !== '__google__docs')
+          res.render('printer.mustache', {
+            printers: printerList,
+            accessToken: response.access_token,
+            refreshToken: response.refresh_token
+          })
         })
-        .catch((error) => res.render('printer.mustache', {printers: []}))
+    })
+    .catch((error) => res.render('printer.mustache', {printers: []}))
 })
 
 app.get('/qr', (req, res) => qrcode.toDataURL(JSON.stringify(req.query), (err, url) => res.render('qr.mustache', {qrcode: url})))
